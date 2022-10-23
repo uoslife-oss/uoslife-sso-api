2eb2dda1b12ba8074abfc6d7c56a3d8553ed4a5f
Junsu Lee <me@adrinerdp.co>
2022-10-23 15:28:03 +0900

    fix: change folder structure to layered architecture

--- src/main.module.ts
+++ src/main.module.ts
@@ -4,15 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { dataSourceConfig } from './data-source';
 
-import { AppModule } from '@app/app.module';
-import { InfrastructureModule } from '@infrastructure/infrastructure.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
     TypeOrmModule.forRoot(dataSourceConfig),
-    AppModule,
-    InfrastructureModule,
   ],
 })
 export class MainModule {}
